ba00e946ec43cab3f04587e65ecb326f9a7ff5df
Eric Haag <ehaag@gradle.com>
2023-04-24 12:53:28 -0500

    Add tests

---
+++ src/test/java/com/gradle/FooTests.java
@@ -0,0 +1,12 @@
+package com.gradle;
+
+import org.junit.jupiter.api.Test;
+
+public class FooTests {
+
+    @Test
+    void test() throws InterruptedException {
+        Thread.sleep(5321);
+    }
+
+}
