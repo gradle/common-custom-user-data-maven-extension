ca6511863bfc41b1eaf95e6155e6ef70e8a105ae
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-02-27 15:24:58 +0100

    Implement `GradleEnterpriseApi` and `DevelocityApi` adapters

---
+++ src/main/java/com/gradle/ccud/adapters/develocity/enterprise/DevelocityApiAdapter.java
@@ -0,0 +1,88 @@
+package com.gradle.ccud.adapters.develocity.enterprise;
+
+import com.gradle.ccud.adapters.BuildCacheApiAdapter;
+import com.gradle.ccud.adapters.BuildScanApiAdapter;
+import com.gradle.ccud.adapters.DevelocityAdapter;
+import com.gradle.develocity.agent.maven.api.DevelocityApi;
+
+import java.net.URI;
+import java.nio.file.Path;
+
+public class DevelocityApiAdapter implements DevelocityAdapter {
+
+    private final DevelocityApi api;
+
+    public DevelocityApiAdapter(DevelocityApi api) {
+        this.api = api;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return api.isEnabled();
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        api.setEnabled(enabled);
+    }
+
+    @Override
+    public void setProjectId(String projectId) {
+        api.setProjectId(projectId);
+    }
+
+    @Override
+    public String getProjectId() {
+        return api.getProjectId();
+    }
+
+    @Override
+    public Path getStorageDirectory() {
+        return api.getStorageDirectory();
+    }
+
+    @Override
+    public void setStorageDirectory(Path path) {
+        api.setStorageDirectory(path);
+    }
+
+    @Override
+    public void setServer(URI url) {
+        api.setServer(url);
+    }
+
+    @Override
+    public String getServer() {
+        return api.getServer();
+    }
+
+    @Override
+    public void setAllowUntrustedServer(boolean allow) {
+        api.setAllowUntrustedServer(allow);
+    }
+
+    @Override
+    public boolean getAllowUntrustedServer() {
+        return api.getAllowUntrustedServer();
+    }
+
+    @Override
+    public void setAccessKey(String accessKey) {
+        api.setAccessKey(accessKey);
+    }
+
+    @Override
+    public String getAccessKey() {
+        return api.getAccessKey();
+    }
+
+    @Override
+    public BuildScanApiAdapter getBuildScan() {
+        return null;
+    }
+
+    @Override
+    public BuildCacheApiAdapter getBuildCache() {
+        return null;
+    }
+}
