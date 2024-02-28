9f42770f07b3352d83f97065f418a3d89544fc60
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-02-28 14:00:40 +0100

    Remove de-duplication logic from the extension

--- src/main/java/com/gradle/CommonCustomUserDataListener.java
+++ src/main/java/com/gradle/CommonCustomUserDataListener.java
@@ -8,35 +8,30 @@ import org.apache.maven.execution.MavenSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 abstract class CommonCustomUserDataListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CommonCustomUserDataListener.class);
-    private static final AtomicBoolean SINGLE_APPLICATION_LOCK = new AtomicBoolean(false);
 
     protected void configure(DevelocityAdapter api, MavenSession session, CustomConfigurationSpec customConfigurationSpec) throws MavenExecutionException {
-        if (SINGLE_APPLICATION_LOCK.compareAndSet(false, true)) {
-            LOGGER.debug("Executing extension: " + getClass().getSimpleName());
-            CustomDevelocityConfig customDevelocityConfig = new CustomDevelocityConfig();
+        LOGGER.debug("Executing extension: " + getClass().getSimpleName());
+        CustomDevelocityConfig customDevelocityConfig = new CustomDevelocityConfig();
 
-            LOGGER.debug("Configuring {}", customConfigurationSpec.displayName);
-            customDevelocityConfig.configureDevelocity(api);
-            LOGGER.debug("Finished configuring {}", customConfigurationSpec.displayName);
+        LOGGER.debug("Configuring {}", customConfigurationSpec.displayName);
+        customDevelocityConfig.configureDevelocity(api);
+        LOGGER.debug("Finished configuring {}", customConfigurationSpec.displayName);
 
-            LOGGER.debug("Configuring build scan publishing and applying build scan enhancements");
-            BuildScanApiAdapter buildScan = api.getBuildScan();
-            customDevelocityConfig.configureBuildScanPublishing(buildScan);
-            new CustomBuildScanEnhancements(buildScan, session).apply();
-            LOGGER.debug("Finished configuring build scan publishing and applying build scan enhancements");
+        LOGGER.debug("Configuring build scan publishing and applying build scan enhancements");
+        BuildScanApiAdapter buildScan = api.getBuildScan();
+        customDevelocityConfig.configureBuildScanPublishing(buildScan);
+        new CustomBuildScanEnhancements(buildScan, session).apply();
+        LOGGER.debug("Finished configuring build scan publishing and applying build scan enhancements");
 
-            LOGGER.debug("Configuring build cache");
-            BuildCacheApiAdapter buildCache = api.getBuildCache();
-            customDevelocityConfig.configureBuildCache(buildCache);
-            LOGGER.debug("Finished configuring build cache");
+        LOGGER.debug("Configuring build cache");
+        BuildCacheApiAdapter buildCache = api.getBuildCache();
+        customDevelocityConfig.configureBuildCache(buildCache);
+        LOGGER.debug("Finished configuring build cache");
 
-            GroovyScriptUserData.evaluate(session, api, LOGGER, customConfigurationSpec);
-        }
+        GroovyScriptUserData.evaluate(session, api, LOGGER, customConfigurationSpec);
     }
 
 }
